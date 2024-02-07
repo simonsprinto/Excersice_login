8badb20454b424f598cc4b8c0c6ba6d8c24dc031
Simon <simpa92@icloud.com>
2024-02-07 13:54:53 +0100

    added search and three users

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ function login() {
     // Återställ felmeddelandet
     errorMessage.innerHTML = "";
 
-    // Kontrollera om användarnamn och lösenord är korrekta
-    if (username === "user" && password === "password") {
+    // Definiera användare med deras användarnamn och lösenord
+    var users = [
+        { username: "user1", password: "password1" },
+        { username: "user2", password: "password2" },
+        { username: "user3", password: "password3" }
+    ];
+
+    // Kontrollera om användarnamn och lösenord är korrekta för någon av användarna
+    var validUser = users.find(function (user) {
+        return user.username === username && user.password === password;
+    });
+
+    if (validUser) {
         // Vidarebefordra till inloggad-sida
         window.location.href = "loggedin.html";
     } else {
